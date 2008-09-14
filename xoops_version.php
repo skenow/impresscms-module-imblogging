0b1c9fe1f807a3af91ee46b324ef1efe6ca6a784
Marc-André Lanciault <marcan@impresscms.org>
2008-09-14 19:19:04 +0000

    Fix in the inclusion of icms_version.php
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imblogging/trunk@4859 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- xoops_version.php
+++ xoops_version.php
@@ -17,7 +17,7 @@ if (!defined("ICMS_ROOT_PATH")) die("ICMS root path not defined");
 /**
  * For now, let's manually include icms_version.php. In ImpressCMS 1.2, this shall be done automatically of course :-)'
  */
-global $modversion;
-include_once(dirname( __FILE__ ) . '/icms_version.php');
+//global $modversion;
+include(dirname( __FILE__ ) . '/icms_version.php');
 
 ?>
