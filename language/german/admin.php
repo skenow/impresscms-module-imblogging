e221804ad55a40bca9051576297eaa8b4bcceef7
xoops-magazine <xoops-magazine@tld.com>
2008-09-18 15:29:23 +0000

    start german language
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imblogging/trunk@4952 7a4d4d67-0c3a-0410-b24c-89d50c169223

---
+++ language/german/admin.php
@@ -0,0 +1,29 @@
+<?php
+/**
+* English language constants used in admin section of the module
+*
+* @copyright	http://smartfactory.ca The SmartFactory
+* @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+* @since		1.0
+* @author		marcan aka Marc-André Lanciault <marcan@smartfactory.ca>
+* @version		$Id$
+*/
+
+if (!defined("ICMS_ROOT_PATH")) die("ICMS root path not defined");
+
+// general
+define("_AM_IMBLOGGING_POSTS", "Posts");
+define("_AM_IMBLOGGING_FIRST_USE", "This is the first time you access this module. Please update the module in order to dynamically create the database scheme.");
+
+// Post
+define("_AM_IMBLOGGING_POSTS_DSC", "All posts in the module");
+define("_AM_IMBLOGGING_POST_CREATE", "Add a post");
+define("_AM_IMBLOGGING_POST", "Post");
+define("_AM_IMBLOGGING_POST_CREATE_INFO", "Fill-out the following form to create a new post.");
+define("_AM_IMBLOGGING_POST_EDIT", "Edit this post");
+define("_AM_IMBLOGGING_POST_EDIT_INFO", "Fill-out the following form in order to edit this post.");
+define("_AM_IMBLOGGING_POST_MODIFIED", "The post was successfully modified.");
+define("_AM_IMBLOGGING_POST_CREATED", "The post has been successfully created.");
+define("_AM_IMBLOGGING_POST_VIEW", "Post info");
+define("_AM_IMBLOGGING_POST_VIEW_DSC", "Here is the info about this post.");
+?>
---
+++ language/german/common.php
@@ -0,0 +1,39 @@
+<?php
+/**
+* English language constants commonly used in the module
+*
+* @copyright	http://smartfactory.ca The SmartFactory
+* @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+* @since		1.0
+* @author		marcan aka Marc-André Lanciault <marcan@smartfactory.ca>
+* @version		$Id$
+*/
+
+if (!defined("ICMS_ROOT_PATH")) die("ICMS root path not defined");
+
+
+// post
+define("_CO_IMBLOGGING_POST_POST_TITLE", "Titel");
+define("_CO_IMBLOGGING_POST_POST_TITLE_DSC", "");
+define("_CO_IMBLOGGING_POST_POST_CONTENT", "Inhalt");
+define("_CO_IMBLOGGING_POST_POST_CONTENT_DSC", "");
+define("_CO_IMBLOGGING_POST_POST_PUBLISHED_DATE", "Datum");
+define("_CO_IMBLOGGING_POST_POST_PUBLISHED_DATE_DSC", "");
+define("_CO_IMBLOGGING_POST_POST_UID", "Poster");
+define("_CO_IMBLOGGING_POST_POST_UID_DSC", "");
+define("_CO_IMBLOGGING_POST_POST_STATUS", "Status");
+define("_CO_IMBLOGGING_POST_POST_STATUS_DSC", "");
+define("_CO_IMBLOGGING_POST_POST_CANCOMMENT", "Can comment?");
+define("_CO_IMBLOGGING_POST_POST_CANCOMMENT_DSC", "");
+
+define("_CO_IMBLOGGING_POST_INFO", "Published by %s on %s.");
+define("_CO_IMBLOGGING_POST_FROM_USER", "All posts of %s");
+define("_CO_IMBLOGGING_POST_COMMENTS_INFO", "%d Kommentar(e)");
+define("_CO_IMBLOGGING_POST_NO_COMMENT", "Kein Kommentar");
+
+// post status
+define("_CO_IMBLOGGING_POST_STATUS_PUBLISHED", "Published");
+define("_CO_IMBLOGGING_POST_STATUS_PENDING", "Pending review");
+define("_CO_IMBLOGGING_POST_STATUS_DRAFT", "Entwurf");
+define("_CO_IMBLOGGING_POST_STATUS_PRIVATE", "Privat");
+?>
