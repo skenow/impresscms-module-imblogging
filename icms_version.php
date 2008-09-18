116467fe80111373dbf57c4df9ecf334833429c0
Steve Keno <skenow@impresscms.org>
2008-09-18 00:48:44 +0000

    Changed structure of the file to be more compact and easier to read (in my opinion). This results in a much smaller file (save over 1K) that is quicker to load.
    I have not been able to install in a directory other than imblogging - perhaps because of what is now lines 74 - 75. I tried modifying them, but did not have any success - I was always in a loop of having to update the module.
    
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imblogging/trunk@4947 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- icms_version.php
+++ icms_version.php
@@ -13,41 +13,33 @@
 
 if (!defined("ICMS_ROOT_PATH")) die("ICMS root path not defined");
 
-/**
- * General Information
- */
-$modversion['name'] = _MI_IMBLOGGING_MD_NAME;
-$modversion['version'] = 1.0;
-$modversion['description'] = _MI_IMBLOGGING_MD_DESC;
-$modversion['author'] = "The SmartFactory";
-$modversion['credits'] = "INBOX International inc.";
-$modversion['help'] = "";
-$modversion['license'] = "GNU General Public License (GPL)";
-$modversion['official'] = 0;
-$modversion['dirname'] = basename( dirname( __FILE__ ) ) ;
+/**  General Information  */
+$modversion = array(
+  'name'=> _MI_IMBLOGGING_MD_NAME,
+  'version'=> 1.0,
+  'description'=> _MI_IMBLOGGING_MD_DESC,
+  'author'=> "The SmartFactory",
+  'credits'=> "INBOX International inc.",
+  'help'=> "",
+  'license'=> "GNU General Public License (GPL)",
+  'official'=> 0,
+  'dirname'=> basename( dirname( __FILE__ ) ),
 
-/**
- * Images information
- */
-$modversion['iconsmall'] = "images/icon_small.png";
-$modversion['iconbig'] = "images/icon_big.png";
-// for backward compatibility
-$modversion['image'] = $modversion['iconbig'];
+/**  Images information  */
+  'iconsmall'=> "images/icon_small.png",
+  'iconbig'=> "images/icon_big.png",
+  'image'=> "images/icon_big.png", /* for backward compatibility */
 
-/**
- * Development information
- */
-$modversion['status_version'] = "Beta 1";
-$modversion['status'] = "Beta";
-$modversion['date'] = "unreleased";
-$modversion['author_word'] = "";
+/**  Development information */
+  'status_version'=> "Beta 1",
+  'status'=> "Beta",
+  'date'=> "unreleased",
+  'author_word'=> "",
 
-/**
- * Contributors
- */
-$modversion['developer_website_url'] = "http://smartfactory.ca";
-$modversion['developer_website_name'] = "The SmartFactory";
-$modversion['developer_email'] = "info@smartfactory.ca";
+/** Contributors */
+  'developer_website_url' => "http://smartfactory.ca",
+  'developer_website_name' => "The SmartFactory",
+  'developer_email' => "info@smartfactory.ca");
 $modversion['people']['developers'][] = "[url=http://smartfactory.ca/userinfo.php?uid=1]marcan[/url] (Marc-Andr&eacute; Lanciault)";
 $modversion['people']['developers'][] = "[url=http://smartfactory.ca/userinfo.php?uid=112]felix[/url] (F&eacute;lix Tousignant)";
 //$modversion['people']['testers'][] = "";
@@ -56,36 +48,26 @@ $modversion['people']['developers'][] = "[url=http://smartfactory.ca/userinfo.ph
 //$modversion['people']['other'][] = "";
 //$modversion['warning'] = _CO_SOBJECT_WARNING_BETA;
 
-/**
- * Administrative information
- */
+/** Administrative information */
 $modversion['hasAdmin'] = 1;
 $modversion['adminindex'] = "admin/index.php";
 $modversion['adminmenu'] = "admin/menu.php";
 
-/**
- * Database information
- */
+/** Database information */
 $modversion['object_items'][1] = 'post';
 $modversion["tables"] = icms_getTablesArray($modversion['dirname'], $modversion['object_items']);
 
-/**
- * Install and update informations
- */
+/** Install and update informations */
 $modversion['onInstall'] = "include/onupdate.inc.php";
 $modversion['onUpdate'] = "include/onupdate.inc.php";
 
-
-/**
- * Search information
- */
+/** Search information */
 $modversion['hasSearch'] = 1;
-$modversion['search']['file'] = "include/search.inc.php";
-$modversion['search']['func'] = "imblogging_search";
+$modversion['search'] = array (
+  'file' => "include/search.inc.php",
+  'func' => "imblogging_search");
 
-/**
- * Menu information
- */
+/** Menu information */
 $modversion['hasMain'] = 1;
 global $xoopsModule;
 if (is_object($xoopsModule) && $xoopsModule->dirname() == 'imblogging') {
@@ -96,113 +78,90 @@ if (is_object($xoopsModule) && $xoopsModule->dirname() == 'imblogging') {
 	}
 }
 
-/**
- * Blocks information
- */
-$i = 0;
+/** Templates information */
+$modversion['templates'][1] = array(
+  'file' => 'imblogging_header.html',
+  'description' => 'Module Header');
 
-/*
-$i++;
-$modversion['blocks'][$i]['file'] = "items_new.php";
-$modversion['blocks'][$i]['name'] = _MI_SSECTION_ITEMSNEW;
-$modversion['blocks'][$i]['description'] = "Shows new items";
-$modversion['blocks'][$i]['show_func'] = "smartsection_items_new_show";
-$modversion['blocks'][$i]['edit_func'] = "smartsection_items_new_edit";
-$modversion['blocks'][$i]['options'] = "0|datesub|5|65";
-$modversion['blocks'][$i]['template'] = "smartsection_items_new.html";
-*/
+$modversion['templates'][] = array( 
+  'file' => 'imblogging_footer.html',
+  'description' => 'Module Footer');
 
-/**
- * Templates information
- */
-$i = 0;
+$modversion['templates'][]= array(
+  'file' => 'imblogging_admin_post.html',
+  'description' => 'Post Index');
 
-$i++;
-$modversion['templates'][$i]['file'] = 'imblogging_header.html';
-$modversion['templates'][$i]['description'] = 'Module Header';
+$modversion['templates'][] = array(
+  'file' => 'imblogging_index.html',
+  'description' => 'Post Index');
 
-$i++;
-$modversion['templates'][$i]['file'] = 'imblogging_footer.html';
-$modversion['templates'][$i]['description'] = 'Module Footer';
+$modversion['templates'][] = array(
+  'file' => 'imblogging_single_post.html',
+  'description' => 'Single post template');
 
-$i++;
-$modversion['templates'][$i]['file'] = 'imblogging_admin_post.html';
-$modversion['templates'][$i]['description'] = 'Post Index';
+$modversion['templates'][] = array(
+  'file' => 'imblogging_post.html',
+  'description' => 'Post page');
 
-$i++;
-$modversion['templates'][$i]['file'] = 'imblogging_index.html';
-$modversion['templates'][$i]['description'] = 'Post Index';
+/** Preferences information */
 
-$i++;
-$modversion['templates'][$i]['file'] = 'imblogging_single_post.html';
-$modversion['templates'][$i]['description'] = 'Single post template';
-
-$i++;
-$modversion['templates'][$i]['file'] = 'imblogging_post.html';
-$modversion['templates'][$i]['description'] = 'Post page';
-
-
-/**
- * Preferences information
- */
-$i = 0;
-
-// Retreive the group user list, because the automatic group_multi config formtype does not include Anonymous group :-(
+// Retrieve the group user list, because the automatic group_multi config formtype does not include Anonymous group :-(
 $member_handler =& xoops_getHandler('member');
 $groups_array = $member_handler->getGroupList();
 foreach($groups_array as $k=>$v) {
 	$select_groups_options[$v] = $k;
 }
-$i++;
-$modversion['config'][$i]['name'] = 'poster_groups';
-$modversion['config'][$i]['title'] = '_MI_IMBLOGGING_POSTERGR';
-$modversion['config'][$i]['description'] = '_MI_IMBLOGGING_POSTERGRDSC';
-$modversion['config'][$i]['formtype'] = 'select_multi';
-$modversion['config'][$i]['valuetype'] = 'array';
-$modversion['config'][$i]['options'] = $select_groups_options;
-$modversion['config'][$i]['default'] =  '1';
 
-$i++;
-$modversion['config'][$i]['name'] = 'posts_limit';
-$modversion['config'][$i]['title'] = '_MI_IMBLOGGING_LIMIT';
-$modversion['config'][$i]['description'] = '_MI_IMBLOGGING_LIMITDSC';
-$modversion['config'][$i]['formtype'] = 'textbox';
-$modversion['config'][$i]['valuetype'] = 'text';
-$modversion['config'][$i]['default'] = 5;
+$modversion['config'][1] = array(
+  'name' => 'poster_groups',
+  'title' => '_MI_IMBLOGGING_POSTERGR',
+  'description' => '_MI_IMBLOGGING_POSTERGRDSC',
+  'formtype' => 'select_multi',
+  'valuetype' => 'array',
+  'options' => $select_groups_options,
+  'default' =>  '1');
 
-/**
- * Comments information
- */
+$modversion['config'][] = array(
+  'name' => 'posts_limit',
+  'title' => '_MI_IMBLOGGING_LIMIT',
+  'description' => '_MI_IMBLOGGING_LIMITDSC',
+  'formtype' => 'textbox',
+  'valuetype' => 'text',
+  'default' => 5);
+
+/** Comments information */
 $modversion['hasComments'] = 1;
 
-$modversion['comments']['itemName'] = 'post_id';
-$modversion['comments']['pageName'] = 'post.php';
+$modversion['comments'] = array(
+  'itemName' => 'post_id',
+  'pageName' => 'post.php',
+  /* Comment callback functions */
+  'callbackFile' => 'include/comment.inc.php',
+  'callback' => array(
+    'approve' => 'imblogging_com_approve',
+    'update' => 'imblogging_com_update')
+    );
 
-// Comment callback functions
-$modversion['comments']['callbackFile'] = 'include/comment.inc.php';
-$modversion['comments']['callback']['approve'] = 'imblogging_com_approve';
-$modversion['comments']['callback']['update'] = 'imblogging_com_update';
-
-
-/**
- * Notification information
- */
+/** Notification information */
 $modversion['hasNotification'] = 1;
 
-$modversion['notification']['lookup_file'] = 'include/notification.inc.php';
-$modversion['notification']['lookup_func'] = 'imblogging_notify_iteminfo';
+$modversion['notification'] = array (
+  'lookup_file' => 'include/notification.inc.php',
+  'lookup_func' => 'imblogging_notify_iteminfo');
 
-$modversion['notification']['category'][1]['name'] = 'global';
-$modversion['notification']['category'][1]['title'] = _MI_IMBLOGGING_GLOBAL_NOTIFY;
-$modversion['notification']['category'][1]['description'] = _MI_IMBLOGGING_GLOBAL_NOTIFY_DSC;
-$modversion['notification']['category'][1]['subscribe_from'] = array('index.php', 'post.php');
+$modversion['notification']['category'][1] = array (
+  'name' => 'global',
+  'title' => _MI_IMBLOGGING_GLOBAL_NOTIFY,
+  'description' => _MI_IMBLOGGING_GLOBAL_NOTIFY_DSC,
+  'subscribe_from' => array('index.php', 'post.php'));
 
-$modversion['notification']['event'][1]['name'] = 'post_published';
-$modversion['notification']['event'][1]['category'] = 'global';
-$modversion['notification']['event'][1]['title'] = _MI_IMBLOGGING_GLOBAL_POST_PUBLISHED_NOTIFY;
-$modversion['notification']['event'][1]['caption'] = _MI_IMBLOGGING_GLOBAL_POST_PUBLISHED_NOTIFY_CAP;
-$modversion['notification']['event'][1]['description'] = _MI_IMBLOGGING_GLOBAL_POST_PUBLISHED_NOTIFY_DSC;
-$modversion['notification']['event'][1]['mail_template'] = 'global_post_published';
-$modversion['notification']['event'][1]['mail_subject'] = _MI_IMBLOGGING_GLOBAL_POST_PUBLISHED_NOTIFY_SBJ;
+$modversion['notification']['event'][1] = array(
+  'name' => 'post_published',
+  'category'=> 'global',
+  'title'=> _MI_IMBLOGGING_GLOBAL_POST_PUBLISHED_NOTIFY,
+  'caption'=> _MI_IMBLOGGING_GLOBAL_POST_PUBLISHED_NOTIFY_CAP,
+  'description'=> _MI_IMBLOGGING_GLOBAL_POST_PUBLISHED_NOTIFY_DSC,
+  'mail_template'=> 'global_post_published',
+  'mail_subject'=> _MI_IMBLOGGING_GLOBAL_POST_PUBLISHED_NOTIFY_SBJ);
 
 ?>
