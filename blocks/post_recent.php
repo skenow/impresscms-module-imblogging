f61db24326dcad3e5c691b0697f64831295e7a28
Marc-André Lanciault <marcan@impresscms.org>
2008-09-19 20:29:46 +0000

    post_recent.php which will handle recent posts block
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imblogging/trunk@5003 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- blocks/post_recent.php
+++ blocks/post_recent.php
@@ -15,13 +15,51 @@ if (!defined("ICMS_ROOT_PATH")) die("ICMS root path not defined");
 
 function imblogging_post_recent_show($options)
 {
+	include_once(ICMS_ROOT_PATH."/modules/imblogging/common.php");
+
+
 
 	return $block;
 }
 
 function imblogging_post_recent_edit($options)
 {
+	include_once(XOOPS_ROOT_PATH."/modules/smartsection/include/functions.php");
 
-}
+	$form .= '
+	<table>
+		<tr>
+			<td style="vertical-align: top; width: 150px;">' . _MB_SSECTION_SELECTCAT . '</td>';
+	$form .= '<td>';
+	$form .= smartsection_createCategorySelect($options[0]) . '</td>';
+
+    $form .= "<tr><td>" . _MB_SSECTION_ORDER . "</td>";
+    $form .= "<td><select name='options[]'>";
+
+    $form .= "<option value='datesub'";
+    if ($options[1] == "datesub") {
+        $form .= " selected='selected'";
+    }
+    $form .= ">" . _MB_SSECTION_DATE . "</option>";
+
+    $form .= "<option value='counter'";
+    if ($options[1] == "counter") {
+        $form .= " selected='selected'";
+    }
+    $form .= ">" . _MB_SSECTION_HITS . "</option>";
+
+    $form .= "<option value='weight'";
+    if ($options[1] == "weight") {
+        $form .= " selected='selected'";
+    }
+    $form .= ">" . _MB_SSECTION_WEIGHT . "</option>";
+
+    $form .= "</select></td>";
+
+    $form .= "</tr><tr><td>" . _MB_SSECTION_DISP . "</td><td><input type='text' name='options[]' value='" . $options[2] . "' />&nbsp;" . _MB_SSECTION_ITEMS . "</td></tr>";
+    $form .= "<tr><td>" . _MB_SSECTION_CHARS . "</td><td><input type='text' name='options[]' value='" . $options[3] . "' />&nbsp;chars</td></tr>";
+	$form .= "</table>";
+    return $form;
+	}
 
 ?>
