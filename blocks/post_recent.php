c6d2c776f3ee654c6b2614568fff4ed9eedf8bfd
Marc-André Lanciault <marcan@impresscms.org>
2008-09-19 20:28:08 +0000

    post_recent.php which will handle recent posts block
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imblogging/trunk@5000 7a4d4d67-0c3a-0410-b24c-89d50c169223

---
+++ blocks/post_recent.php
@@ -0,0 +1,27 @@
+<?php
+/**
+* Recent posts block file
+*
+* This file holds the functions needed for the recent posts block
+*
+* @copyright	http://smartfactory.ca The SmartFactory
+* @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+* @since		1.0
+* @author		marcan aka Marc-André Lanciault <marcan@smartfactory.ca>
+* @version		$Id$
+*/
+
+if (!defined("ICMS_ROOT_PATH")) die("ICMS root path not defined");
+
+function imblogging_post_recent_show($options)
+{
+
+	return $block;
+}
+
+function imblogging_post_recent_edit($options)
+{
+
+}
+
+?>
