c8f7a87551d7aa3f297dc7f776806d7e2178ca2d
GibaPhp <gibaphp.xoopstotal@gmail.com>
2008-09-25 18:34:47 +0000

    imBlogging - portuguesebr - Step 2 - finished translation, falt revision...
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imblogging/trunk@5105 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- language/portuguesebr/admin.php
+++ language/portuguesebr/admin.php
@@ -1,29 +1,30 @@
-<?php
+﻿<?php
 /**
-* English language constants used in admin section of the module
+*  Portuguese language constants used in admin section of the module
 *
 * @copyright	http://smartfactory.ca The SmartFactory
 * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
 * @since		1.0
 * @author		marcan aka Marc-André Lanciault <marcan@smartfactory.ca>
 * @version		$Id$
+* @translation        GibaPhp - http://br.impresscms.org 
 */
 
-if (!defined("ICMS_ROOT_PATH")) die("ICMS root path not defined");
+if (!defined("ICMS_ROOT_PATH")) die("O caminho para o raiz do site não foi definido");
 
 // general
-define("_AM_IMBLOGGING_POSTS", "Posts");
-define("_AM_IMBLOGGING_FIRST_USE", "This is the first time you access this module. Please update the module in order to dynamically create the database scheme.");
+define("_AM_IMBLOGGING_POSTS", "Blogs");
+define("_AM_IMBLOGGING_FIRST_USE", "Esta é a primeira vez que você está entrando neste módulo. Atualize o módulo imediatamente para criar a sua base de dados padrão.");
 
 // Post
-define("_AM_IMBLOGGING_POSTS_DSC", "All posts in the module");
-define("_AM_IMBLOGGING_POST_CREATE", "Add a post");
-define("_AM_IMBLOGGING_POST", "Post");
-define("_AM_IMBLOGGING_POST_CREATE_INFO", "Fill-out the following form to create a new post.");
-define("_AM_IMBLOGGING_POST_EDIT", "Edit this post");
-define("_AM_IMBLOGGING_POST_EDIT_INFO", "Fill-out the following form in order to edit this post.");
-define("_AM_IMBLOGGING_POST_MODIFIED", "The post was successfully modified.");
-define("_AM_IMBLOGGING_POST_CREATED", "The post has been successfully created.");
-define("_AM_IMBLOGGING_POST_VIEW", "Post info");
-define("_AM_IMBLOGGING_POST_VIEW_DSC", "Here is the info about this post.");
+define("_AM_IMBLOGGING_POSTS_DSC", "Todos os Blogs do módulo");
+define("_AM_IMBLOGGING_POST_CREATE", "Incluir um Blog");
+define("_AM_IMBLOGGING_POST", "Blog");
+define("_AM_IMBLOGGING_POST_CREATE_INFO", "Preencher o formulário para criar um novo Blog.");
+define("_AM_IMBLOGGING_POST_EDIT", "Editar este Blog");
+define("_AM_IMBLOGGING_POST_EDIT_INFO", "Preencha o seguinte formulário para editar este Blog.");
+define("_AM_IMBLOGGING_POST_MODIFIED", "O Blog foi modificado com sucesso.");
+define("_AM_IMBLOGGING_POST_CREATED", "O Blog foi criado com sucesso.");
+define("_AM_IMBLOGGING_POST_VIEW", "Blog info");
+define("_AM_IMBLOGGING_POST_VIEW_DSC", "Aqui está a informação sobre este Blog.");
 ?>
