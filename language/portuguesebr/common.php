7e323821727efab7ec2c918b549fea5b6c228ba4
GibaPhp <gibaphp.xoopstotal@gmail.com>
2008-09-25 18:37:45 +0000

    imBlogging - portuguesebr - Step 4 - finished translation, falt revision...
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imblogging/trunk@5109 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- language/portuguesebr/common.php
+++ language/portuguesebr/common.php
@@ -1,40 +1,41 @@
 <?php
 /**
-* English language constants commonly used in the module
+*  language constants commonly used in the module
 *
 * @copyright	http://smartfactory.ca The SmartFactory
 * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
 * @since		1.0
 * @author		marcan aka Marc-André Lanciault <marcan@smartfactory.ca>
 * @version		$Id$
+* @translation        GibaPhp - http://br.impresscms.org 
 */
 
-if (!defined("ICMS_ROOT_PATH")) die("ICMS root path not defined");
+if (!defined("ICMS_ROOT_PATH")) die("O caminho para o raiz do site não foi definido");
 
 
-// post
-define("_CO_IMBLOGGING_POST_POST_TITLE", "Title");
+// post - Blog
+define("_CO_IMBLOGGING_POST_POST_TITLE", "Título");
 define("_CO_IMBLOGGING_POST_POST_TITLE_DSC", "");
-define("_CO_IMBLOGGING_POST_POST_CONTENT", "Content");
+define("_CO_IMBLOGGING_POST_POST_CONTENT", "Conteúdo");
 define("_CO_IMBLOGGING_POST_POST_CONTENT_DSC", "");
-define("_CO_IMBLOGGING_POST_POST_PUBLISHED_DATE", "Date");
+define("_CO_IMBLOGGING_POST_POST_PUBLISHED_DATE", "Data");
 define("_CO_IMBLOGGING_POST_POST_PUBLISHED_DATE_DSC", "");
-define("_CO_IMBLOGGING_POST_POST_UID", "Poster");
+define("_CO_IMBLOGGING_POST_POST_UID", "Enviado por");
 define("_CO_IMBLOGGING_POST_POST_UID_DSC", "");
 define("_CO_IMBLOGGING_POST_POST_STATUS", "Status");
 define("_CO_IMBLOGGING_POST_POST_STATUS_DSC", "");
-define("_CO_IMBLOGGING_POST_POST_CANCOMMENT", "Can comment ?");
+define("_CO_IMBLOGGING_POST_POST_CANCOMMENT", "Pode Comentar ?");
 define("_CO_IMBLOGGING_POST_POST_CANCOMMENT_DSC", "");
 
-define("_CO_IMBLOGGING_POST_INFO", "Published by %s on %s.");
-define("_CO_IMBLOGGING_POST_FROM_USER", "All posts of %s");
-define("_CO_IMBLOGGING_POST_FROM_MONTH", "All posts made in %s %u");
-define("_CO_IMBLOGGING_POST_COMMENTS_INFO", "%d comments");
-define("_CO_IMBLOGGING_POST_NO_COMMENT", "No comment");
+define("_CO_IMBLOGGING_POST_INFO", "Publicado por %s no %s.");
+define("_CO_IMBLOGGING_POST_FROM_USER", "Todos Blogs de %s");
+define("_CO_IMBLOGGING_POST_FROM_MONTH", "Todos Blogs feitos no in %s %u");
+define("_CO_IMBLOGGING_POST_COMMENTS_INFO", "%d comentários");
+define("_CO_IMBLOGGING_POST_NO_COMMENT", "Nenhum comentário");
 
 // post status
-define("_CO_IMBLOGGING_POST_STATUS_PUBLISHED", "Published");
-define("_CO_IMBLOGGING_POST_STATUS_PENDING", "Pending review");
-define("_CO_IMBLOGGING_POST_STATUS_DRAFT", "Draft");
-define("_CO_IMBLOGGING_POST_STATUS_PRIVATE", "Private");
+define("_CO_IMBLOGGING_POST_STATUS_PUBLISHED", "Publicado");
+define("_CO_IMBLOGGING_POST_STATUS_PENDING", "Pendente");
+define("_CO_IMBLOGGING_POST_STATUS_DRAFT", "Rascunho");
+define("_CO_IMBLOGGING_POST_STATUS_PRIVATE", "Privado");
 ?>
