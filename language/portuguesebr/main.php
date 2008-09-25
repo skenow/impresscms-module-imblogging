2b243ce4869c763d786a0dae3808b1f2a5a9fe1f
GibaPhp <gibaphp.xoopstotal@gmail.com>
2008-09-25 18:38:33 +0000

    imBlogging - portuguesebr - Step 5 - finished translation, falt revision...
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imblogging/trunk@5110 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- language/portuguesebr/main.php
+++ language/portuguesebr/main.php
@@ -1,21 +1,22 @@
 <?php
 /**
-* English language constants used in the user side of the module
+* Portuguese language constants used in the user side of the module
 *
 * @copyright	http://smartfactory.ca The SmartFactory
 * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
 * @since		1.0
 * @author		marcan aka Marc-André Lanciault <marcan@smartfactory.ca>
 * @version		$Id$
+* @translation        GibaPhp - http://br.impresscms.org 
 */
 
-if (!defined("ICMS_ROOT_PATH")) die("ICMS root path not defined");
+if (!defined("ICMS_ROOT_PATH")) die("O caminho para o raiz do site não foi definido");
 
 
-define('_MD_IMBLOGGING_ADMIN_PAGE', ':: Admin page ::');
-define('_MD_IMBLOGGING_POST_EDIT', 'Edit post');
-define('_MD_IMBLOGGING_POST_SUBMIT', 'Submit post');
-define('_MD_IMBLOGGING_POST_MODIFIED', 'The post was successfully modified.');
-define('_MD_IMBLOGGING_POST_CREATED', 'The post has been successfully created.');
-define('_MD_IMBLOGGING_SECURITY_CHECK_FAILED', 'Security check failed...');
+define('_MD_IMBLOGGING_ADMIN_PAGE', ':: Administração ::');
+define('_MD_IMBLOGGING_POST_EDIT', 'Editar Blog');
+define('_MD_IMBLOGGING_POST_SUBMIT', 'Enviar Blog');
+define('_MD_IMBLOGGING_POST_MODIFIED', 'O Blog foi corretamente modificado.');
+define('_MD_IMBLOGGING_POST_CREATED', 'O Blog foi criado corretamente.');
+define('_MD_IMBLOGGING_SECURITY_CHECK_FAILED', 'Verificação de Segurança falhou...');
 ?>
