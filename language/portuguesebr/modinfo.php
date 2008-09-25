6ff9e4bb5a99c3353df10c2893c557f528c57adb
GibaPhp <gibaphp.xoopstotal@gmail.com>
2008-09-25 18:39:25 +0000

    imBlogging - portuguesebr - Step 6 - finished translation, falt revision...
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imblogging/trunk@5111 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- language/portuguesebr/modinfo.php
+++ language/portuguesebr/modinfo.php
@@ -1,12 +1,13 @@
 <?php
 /**
-* English language constants related to module information
+*  Portuguese language constants related to module information
 *
 * @copyright	http://smartfactory.ca The SmartFactory
 * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
 * @since		1.0
 * @author		marcan aka Marc-André Lanciault <marcan@smartfactory.ca>
 * @version		$Id$
+* @translation        GibaPhp - http://br.impresscms.org 
 */
 
 if (!defined("ICMS_ROOT_PATH")) die("O caminho para o raiz do site não foi definido");
@@ -18,7 +19,7 @@ global $xoopsModule;
 define("_MI_IMBLOGGING_MD_NAME", "imBlogging");
 define("_MI_IMBLOGGING_MD_DESC", "Módulo simples de Blog para o ImpressCMS");
 
-define("_MI_IMBLOGGING_POSTS", "Mensagens");
+define("_MI_IMBLOGGING_POSTS", "Blogs");
 
 // Configs
 define("_MI_IMBLOGGING_POSTERGR", "Grupos permitidos para enviar Blogs");
