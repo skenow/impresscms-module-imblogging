c1225c0b854a60f5570defc0896d236ee7e425a6
GibaPhp <gibaphp.xoopstotal@gmail.com>
2008-09-25 18:36:45 +0000

    imBlogging - portuguesebr - Step 3 - finished translation, falt revision...
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imblogging/trunk@5107 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- language/portuguesebr/blocks.php
+++ language/portuguesebr/blocks.php
@@ -1,15 +1,16 @@
 <?php
 /**
-* English language constants used in blocks of the module
+*  Portuguese language constants used in blocks of the module
 *
 * @copyright	http://smartfactory.ca The SmartFactory
 * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
 * @since		1.0
 * @author		marcan aka Marc-André Lanciault <marcan@smartfactory.ca>
 * @version		$Id$
+* @translation        GibaPhp - http://br.impresscms.org 
 */
 
-if (!defined("ICMS_ROOT_PATH")) die("ICMS root path not defined");
+if (!defined("ICMS_ROOT_PATH")) die("O caminho para o raiz do site não foi definido");
 
-define("_MB_IMBLOGGING_POST_RECENT_LIMIT", "Post limit: ");
+define("_MB_IMBLOGGING_POST_RECENT_LIMIT", "Limite de Blogs: ");
 ?>
