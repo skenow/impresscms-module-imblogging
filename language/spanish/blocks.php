240e09ba1da5f8d0d52f6cc22c4ab31583de4c6d
Juan Calzado <debianus@impresscms.org>
2008-10-13 12:24:57 +0000

    Spanish translation for imBlogging finished
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imblogging/tags/imblogging_1.0_rc1@5636 7a4d4d67-0c3a-0410-b24c-89d50c169223

---
+++ language/spanish/blocks.php
@@ -0,0 +1,15 @@
+<?php
+/**
+* Spanish language constants used in admin section of the module (traducción por debianus)
+*
+* @copyright	http://smartfactory.ca The SmartFactory
+* @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+* @since		1.0
+* @author		marcan aka Marc-André Lanciault <marcan@smartfactory.ca>
+* @version		$Id$
+*/
+
+if (!defined("ICMS_ROOT_PATH")) die("ICMS root path not defined");
+
+define("_MB_IMBLOGGING_POST_RECENT_LIMIT", "Máximo número para el bloque de artículos recientes: ");
+?>
---
+++ language/spanish/common.php
@@ -0,0 +1,46 @@
+<?php
+/**
+* * Spanish language constants used in admin section of the module (traducción por debianus)
+*
+* @copyright	http://smartfactory.ca The SmartFactory
+* @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+* @since		1.0
+* @author		marcan aka Marc-André Lanciault <marcan@smartfactory.ca>
+* @version		$Id$
+*/
+
+if (!defined("ICMS_ROOT_PATH")) die("ICMS root path not defined");
+
+
+// post
+define("_CO_IMBLOGGING_POST_POST_TITLE", "Título");
+define("_CO_IMBLOGGING_POST_POST_TITLE_DSC", "");
+define("_CO_IMBLOGGING_POST_POST_CONTENT", "Contenido");
+define("_CO_IMBLOGGING_POST_POST_CONTENT_DSC", "");
+define("_CO_IMBLOGGING_POST_POST_PUBLISHED_DATE", "Fecha");
+define("_CO_IMBLOGGING_POST_POST_PUBLISHED_DATE_DSC", "");
+define("_CO_IMBLOGGING_POST_POST_UID", "Autor");
+define("_CO_IMBLOGGING_POST_POST_UID_DSC", "");
+define("_CO_IMBLOGGING_POST_POST_STATUS", "Estado");
+define("_CO_IMBLOGGING_POST_POST_STATUS_DSC", "");
+define("_CO_IMBLOGGING_POST_POST_CANCOMMENT", "¿Admitir comentarios?");
+define("_CO_IMBLOGGING_POST_POST_CANCOMMENT_DSC", "");
+define("_CO_IMBLOGGING_POST_ALL_POSTS", "Todos los artículos");
+
+define("_CO_IMBLOGGING_POST_INFO", "Publicado por %s el %s.");
+define("_CO_IMBLOGGING_POST_FROM_USER", "Todos los artículos de %s");
+define("_CO_IMBLOGGING_POST_FROM_MONTH", "Todos los artículos publicados en %s %u");
+define("_CO_IMBLOGGING_POST_COMMENTS_INFO", "%d comentarios");
+define("_CO_IMBLOGGING_POST_NO_COMMENT", "No es posible enviar comentarios");
+
+// post status
+define("_CO_IMBLOGGING_POST_STATUS_PUBLISHED", "Publicado");
+define("_CO_IMBLOGGING_POST_STATUS_PENDING", "Pendiente de revisión");
+define("_CO_IMBLOGGING_POST_STATUS_DRAFT", "Proyecto");
+define("_CO_IMBLOGGING_POST_STATUS_PRIVATE", "Privado");
+
+// common language
+define("_CO_IMBLOGGING_POST_PUBLISHED", "Publicado");
+define("_CO_IMBLOGGING_POST_ON", "el");
+define("_CO_IMBLOGGING_POST_BY", "por");
+?>
