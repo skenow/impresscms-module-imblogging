8c6c1f9fd8af9de992475ea7d73406a4fa9def68
Juan Calzado <debianus@impresscms.org>
2008-10-13 12:24:57 +0000

    Spanish translation for imBlogging finished
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imblogging/trunk@5636 7a4d4d67-0c3a-0410-b24c-89d50c169223

---
+++ language/spanish/admin.php
@@ -0,0 +1,29 @@
+<?php
+/**
+* Spanish language constants used in admin section of the module (traducción por debianus)
+*
+* @copyright	http://smartfactory.ca The SmartFactory
+* @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+* @since		1.0
+* @author		marcan aka Marc-André Lanciault <marcan@smartfactory.ca>
+* @version		$Id$
+*/
+
+if (!defined("ICMS_ROOT_PATH")) die("ICMS root path not defined");
+
+// general
+define("_AM_IMBLOGGING_POSTS", "Artículos");
+define("_AM_IMBLOGGING_FIRST_USE", "Esta es la primera vez que accede al módulo. Por favor, actualízelo para crear dinámicamente el esquema en la base de datos.");
+
+// Post
+define("_AM_IMBLOGGING_POSTS_DSC", "Todos los artículos en el módulo");
+define("_AM_IMBLOGGING_POST_CREATE", "Añadir un artículo");
+define("_AM_IMBLOGGING_POST", "Artículo");
+define("_AM_IMBLOGGING_POST_CREATE_INFO", "Complete el siguiente formulario para crear un nuevo artículo.");
+define("_AM_IMBLOGGING_POST_EDIT", "Modificar este artículo");
+define("_AM_IMBLOGGING_POST_EDIT_INFO", "Complete el siguiente formulario para modificar este nuevo artículo.");
+define("_AM_IMBLOGGING_POST_MODIFIED", "El artículo fue modificado con éxito.");
+define("_AM_IMBLOGGING_POST_CREATED", "El artículo fue creado con éxito.");
+define("_AM_IMBLOGGING_POST_VIEW", "Información del artículo");
+define("_AM_IMBLOGGING_POST_VIEW_DSC", "Esta es la información sobre el artículo.");
+?>
---
+++ language/spanish/modinfo.php
@@ -0,0 +1,45 @@
+<?php
+/**
+* * Spanish language constants used in admin section of the module (traducción por debianus)
+*
+* @copyright	http://smartfactory.ca The SmartFactory
+* @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+* @since		1.0
+* @author		marcan aka Marc-André Lanciault <marcan@smartfactory.ca>
+* @version		$Id$
+*/
+
+if (!defined("ICMS_ROOT_PATH")) die("ICMS root path not defined");
+
+// Module Info
+// The name of this module
+
+global $xoopsModule;
+define("_MI_IMBLOGGING_MD_NAME", "imBlogging");
+define("_MI_IMBLOGGING_MD_DESC", "Módulo de creación de blogs para ImpressCMS");
+
+define("_MI_IMBLOGGING_artículos", "Artículos");
+
+// Configs
+define("_MI_IMBLOGGING_POSTERGR", "Grupos a los que se permite publicar artículos");
+define("_MI_IMBLOGGING_POSTERGRDSC", "Seleccione los grupos de usuarios que pueden publicar nuevos artículos. Tenga en cuenta que un usuario perteneciente a cualquiera de los grupos que elija podrá publicar directamente en el sitio web y que el módulo actualmente no tiene la característica de moderar los envios..");
+define("_MI_IMBLOGGING_LIMIT", "Límite de artículos");
+define("_MI_IMBLOGGING_LIMITDSC", "Número de artículos que se mostrarán en el sitio.");
+
+// Blocks
+define("_MI_IMBLOGGING_POSTRECENT", "Artículos recientes");
+define("_MI_IMBLOGGING_POSTRECENTDSC", "Mostrar los artículos más recientes");
+define("_MI_IMBLOGGING_POSTBYMONTH", "Artículos por mes");
+define("_MI_IMBLOGGING_POSTBYMONTHDSC", "Muestra la lista con los meses en los cuales se publicaron artículos");
+
+// Notifications
+define("_MI_IMBLOGGING_GLOBAL_NOTIFY", "Todos los artículos");
+define("_MI_IMBLOGGING_GLOBAL_NOTIFY_DSC", "Notificaciones relacionadas con todos los artículos existentes en el módulo");
+define("_MI_IMBLOGGING_GLOBAL_POST_PUBLISHED_NOTIFY", "Nuevo artículo publicado");
+define("_MI_IMBLOGGING_GLOBAL_POST_PUBLISHED_NOTIFY_CAP", "Notificarme cuando un nuevo artículo es publicado");
+define("_MI_IMBLOGGING_GLOBAL_POST_PUBLISHED_NOTIFY_DSC", "Recibir notificación cuando cualquier nuevo artículo es publicado.");
+define("_MI_IMBLOGGING_GLOBAL_POST_PUBLISHED_NOTIFY_SBJ", "[{X_SITENAME}] {X_MODULE} auto-notify : Nuevo artículo publicado");
+
+// Submit button
+define("_MI_IMBLOGGING_POST_ADD", "Añadir un nuevo artículo");
+?>
