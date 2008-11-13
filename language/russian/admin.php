d0e28e57bf0e1630db35a05db12f1ed91c1103fa
algalochkin <contact@impresscms.org>
2008-11-13 06:11:25 +0000

    5 new definition added (Requirements)
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imblogging/trunk@6809 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- language/russian/admin.php
+++ language/russian/admin.php
@@ -11,6 +11,13 @@
 
 if (!defined("ICMS_ROOT_PATH")) die("Не определен корневой путь к ICMS");
 
+// Requirements
+define("_AM_IMBLOGGING_REQUIREMENTS", "Требования imBlogging");
+define("_AM_IMBLOGGING_REQUIREMENTS_INFO", "Мы просмотрели Вашу систему, к сожалению она не содержит всего необходимого для функционирования модуля imBlogging. Ниже представлены необходимые требования.");
+define("_AM_IMBLOGGING_REQUIREMENTS_ICMS_BUILD", "Для правильного функционирования модуля imBlogging требуется версия не ниже ImpressCMS 1.1.1 RC 1.");
+define("_AM_IMBLOGGING_REQUIREMENTS_IMTAGGING", "Для функционирования модуля imBlogging необходимо установить модуль imTagging. Пожалуйста, возьмите этот модуль в ImpressCMS SVN-депозитарии.");
+define("_AM_IMBLOGGING_REQUIREMENTS_SUPPORT", "Если у Вас имеются вопросы или пожелания, пожалуйста, посетите наш форум на сайте <a href='http://community.impresscms.org'>http://community.impresscms.org</a>.");
+
 // general
 define("_AM_IMBLOGGING_POSTS", "Сообщения");
 define("_AM_IMBLOGGING_FIRST_USE", "Первый вход в этот модуль. Пожалуйста, обновите модуль, чтобы динамически создать схему базы данных.");
