9e82a61ebaa064a139c52f8534bf390df5aebc57
Steve Keno <skenow@impresscms.org>
2012-09-21 04:02:21 +0000

    Formatting and code syntax/style updates to match current standards
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imblogging/trunk@24452 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- blocks/post_recent.php
+++ blocks/post_recent.php
@@ -5,10 +5,10 @@
  * This file holds the functions needed for the recent posts block
  *
  * @copyright	http://smartfactory.ca The SmartFactory
- * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+ * @license	http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
  * @since		1.0
  * @author		marcan aka Marc-André Lanciault <marcan@smartfactory.ca>
- * @version		$Id$
+ * @version	$Id$
  */
 
 if (!defined("ICMS_ROOT_PATH")) die("ICMS root path not defined");
