07bc8f4a8494958fabe2fbb9089fd69e3f80ce8b
Steve Keno <skenow@impresscms.org>
2012-09-21 04:01:33 +0000

    Formatting and code syntax/style updates to match current standards
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imblogging/trunk@24451 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- blocks/post_by_month.php
+++ blocks/post_by_month.php
@@ -1,36 +1,32 @@
 <?php
 /**
-* Posts by month block file
-*
-* This file holds the functions needed for the posts by month block
-*
-* @copyright	http://smartfactory.ca The SmartFactory
-* @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
-* @since		1.0
-* @author		marcan aka Marc-André Lanciault <marcan@smartfactory.ca>
-* @version		$Id$
-*/
+ * Posts by month block file
+ *
+ * This file holds the functions needed for the posts by month block
+ *
+ * @copyright	http://smartfactory.ca The SmartFactory
+ * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+ * @since		1.0
+ * @author		marcan aka Marc-André Lanciault <marcan@smartfactory.ca>
+ * @version		$Id$
+ */
 
 if (!defined("ICMS_ROOT_PATH")) die("ICMS root path not defined");
 
-function imblogging_post_by_month_show($options)
-{
-	include_once(ICMS_ROOT_PATH . '/modules/imblogging/include/common.php');
+function imblogging_post_by_month_show($options) {
+	include_once ICMS_ROOT_PATH . '/modules/imblogging/include/common.php';
 	$imblogging_post_handler = icms_getModuleHandler('post', 'imblogging');
 	$block['posts_by_months'] = $imblogging_post_handler->getPostsCountByMonth();
 
 	return $block;
 }
 
-function imblogging_post_by_month_edit($options)
-{
-	include_once(ICMS_ROOT_PATH . '/modules/imblogging/include/common.php');
+function imblogging_post_by_month_edit($options) {
+	include_once ICMS_ROOT_PATH . '/modules/imblogging/include/common.php';
 
 	$form = '<table><tr>';
 	$form .= '<td>' . _MB_IMBLOGGING_POST_RECENT_LIMIT . '</td>';
-	$form .= '<td>' . '<input type="text" name="options[]" value="' .$options[0] . '"/></td>';
+	$form .= '<td>' . '<input type="text" name="options[]" value="' . $options[0] . '"/></td>';
 	$form .= '</tr></table>';
-    return $form;
-	}
-
-?>
+	return $form;
+}
--- blocks/post_recent.php
+++ blocks/post_recent.php
@@ -1,36 +1,32 @@
 <?php
 /**
-* Recent posts block file
-*
-* This file holds the functions needed for the recent posts block
-*
-* @copyright	http://smartfactory.ca The SmartFactory
-* @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
-* @since		1.0
-* @author		marcan aka Marc-André Lanciault <marcan@smartfactory.ca>
-* @version		$Id$
-*/
+ * Recent posts block file
+ *
+ * This file holds the functions needed for the recent posts block
+ *
+ * @copyright	http://smartfactory.ca The SmartFactory
+ * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+ * @since		1.0
+ * @author		marcan aka Marc-André Lanciault <marcan@smartfactory.ca>
+ * @version		$Id$
+ */
 
 if (!defined("ICMS_ROOT_PATH")) die("ICMS root path not defined");
 
-function imblogging_post_recent_show($options)
-{
-	include_once(ICMS_ROOT_PATH . '/modules/imblogging/include/common.php');
+function imblogging_post_recent_show($options) {
+	include_once ICMS_ROOT_PATH . '/modules/imblogging/include/common.php';
 	$imblogging_post_handler = icms_getModuleHandler('post', 'imblogging');
 	$block['posts'] = $imblogging_post_handler->getPosts(0, $options[0]);
 
 	return $block;
 }
 
-function imblogging_post_recent_edit($options)
-{
-	include_once(ICMS_ROOT_PATH . '/modules/imblogging/include/common.php');
+function imblogging_post_recent_edit($options) {
+	include_once ICMS_ROOT_PATH . '/modules/imblogging/include/common.php';
 
 	$form = '<table><tr>';
 	$form .= '<td>' . _MB_IMBLOGGING_POST_RECENT_LIMIT . '</td>';
-	$form .= '<td>' . '<input type="text" name="options[]" value="' .$options[0] . '"/></td>';
+	$form .= '<td>' . '<input type="text" name="options[]" value="' . $options[0] . '"/></td>';
 	$form .= '</tr></table>';
-    return $form;
-	}
-
-?>
+	return $form;
+}
