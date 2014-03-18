60d522d0818aa7de0cf26a3e0a4700b3bf4a8880
Steve Keno <skenow@impresscms.org>
2014-03-18 13:37:37 +0000

    Adding a new comment had 'test...' inserted in the text displayed.
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imblogging/trunk@25283 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- comment_new.php
+++ comment_new.php
@@ -18,11 +18,13 @@ if ($com_itemid > 0) {
 	$postObj = $imblogging_post_handler->get($com_itemid);
 	if ($postObj && !$postObj->isNew()) {
 		//$com_replytext = _POSTEDBY.'&nbsp;<b>'.smartsection_getLinkedUnameFromId($itemObj->uid()) . '</b>&nbsp;'._DATE.'&nbsp;<b>'.$itemObj->dateSub().'</b><br /><br />'.$itemObj->summary();
-		$com_replytext = 'test...';
+		//$com_replytext = 'test...';
 		$bodytext = $postObj->getPostLead();
+		/*
 		if ($bodytext != '') {
 			$com_replytext .= '<br /><br />'.$bodytext.'';
 		}
+		*/
 		$com_replytitle = $postObj->getVar('post_title');
 		include_once ICMS_ROOT_PATH .'/include/comment_new.php';
 	}
